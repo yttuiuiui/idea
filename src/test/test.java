bfe1777ae3b2a5d58a120536e7c76fcc4bd79f3c
aa <bb>
2017-08-22 10:36:26 +0800

    增加了一个int

--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,7 @@ package test;
  */
 public class test {
     int g=3;
+    int f=9;
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("oiu");
