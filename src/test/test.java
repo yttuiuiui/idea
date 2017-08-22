d65a224f56c9997491eec5d409b1c6a15918b7c2
aa <bb>
2017-08-22 10:29:59 +0800

    添加了一个main方法

---
+++ src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/8/22 0022.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
